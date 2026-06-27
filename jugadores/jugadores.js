b4234d3a1639931b9fd0813c88f2d476634f83e3
CesarGualpa <cesar_amagu@hotmail.com>
2026-06-27 16:02:14 -0500

    feat: arreglo jugadores.

--- jugadores/jugadores.js
+++ jugadores/jugadores.js
@@ -1,1 +1,20 @@
 const cromosMundial = [];
+
+function crearJugador(id, nombre, pais, posicion, urlImagen, urlBandera, colorFondoHex, goles, partidos, destacado, curiosidad) {
+    return {
+        id: id,
+        nombre: nombre,
+        pais: pais,
+        posicion: posicion,
+        urlImagen: urlImagen,
+        urlBandera: urlBandera,
+        colorFondoHex: colorFondoHex,
+        estadisticas: {
+            goles: goles,
+            partidos: partidos
+        },
+        destacado: destacado,
+        curiosidad: curiosidad,
+        desbloqueado: false
+    };
+}
